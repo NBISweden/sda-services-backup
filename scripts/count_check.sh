883d76f8427f655e4990460286b21411cfec0a1d
jonandernovella <novella.uu@gmail.com>
2020-11-12 18:07:24 +0100

    Add integration tests
    They should cover the basic functionality of the tool.

---
+++ scripts/count_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COUNT_1=$(curl http://127.0.0.1:9201/index123-test/_count | jq .count)
+COUNT_2=$(curl http://127.0.0.1:9201/index123-test/_count | jq .count)
+
+echo "${COUNT_1}"
+echo "${COUNT_2}"
+
+if [ $COUNT_1 -ne $COUNT_2 ]; then
+  echo "Something went wrong, the imported backup does not contain all documents"
+  exit 1
+else
+  echo "Backup was successful"
+  exit 0
+fi
---
+++ scripts/readiness.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RETRY_TIMES=0
+for p in elastic elastic2 mys3
+do
+  until [ $(docker inspect --format "{{json .State.Health.Status }}" $p) = "\"healthy\"" ]
+  do
+    echo "Waiting for $p to become ready"
+    docker logs $p | grep -i  "ERROR"
+    RETRY_TIMES=$((RETRY_TIMES+1));
+    echo $RETRY_TIMES
+    if [ $RETRY_TIMES -eq 30 ]; then
+      exit 1;
+    fi
+    sleep 10;
+  done
+  echo "Finished waiting for $p"
+done
