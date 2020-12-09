8820208f91ebb97e22a223571b3d15b0adb6e0da
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2020-12-09 14:49:54 +0100

    Add function to build the DB connection URI
    Now the connection URI will be set up correctly depending on which sslmode is set

--- postgres.go
+++ postgres.go
@@ -27,7 +27,7 @@ type DBConf struct {
 
 func pgDump(sb s3Backend, db DBConf, keyPath string) {
 	today := time.Now().Format("20060102150405")
-	dbURI := fmt.Sprintf("--dbname=postgresql://%s:%s@%s:%d/%s", db.user, db.password, db.host, db.port, db.database)
+	dbURI := buildConnInfo(db)
 	cmd := exec.Command("pg_dump", dbURI, "-xF", "tar")
 
 	var out bytes.Buffer
@@ -106,4 +106,46 @@ func pgRestore(sb s3Backend, db DBConf, keyPath, sqlDump string) {
 	if err != nil {
 		log.Fatalf(errMsg.String())
 	}
+}
+
+// buildConnInfo builds a connection string for the database
+func buildConnInfo(db DBConf) string {
+	dbURI := fmt.Sprintf("--dbname=postgresql://%s:%s@%s:%d/%s", db.user, db.password, db.host, db.port, db.database)
+
+	var certsRequired bool
+
+	switch db.sslMode {
+	case "allow":
+		certsRequired = false
+		dbURI += fmt.Sprintf("?sslmode=%s", db.sslMode)
+	case "disable":
+		certsRequired = false
+		dbURI += fmt.Sprintf("?sslmode=%s", db.sslMode)
+	case "prefer":
+		certsRequired = false
+	case "require":
+		certsRequired = true
+	case "verify-ca":
+		certsRequired = true
+	case "verify-peer":
+		certsRequired = true
+	}
+
+	if certsRequired {
+		dbURI += fmt.Sprintf("?sslmode=%s", db.sslMode)
+
+		if db.caCert != "" {
+			dbURI += fmt.Sprintf("&sslrootcert=%s", db.caCert)
+		}
+
+		if db.clientCert != "" {
+			dbURI += fmt.Sprintf("&sslcert=%s", db.clientCert)
+		}
+
+		if db.clientKey != "" {
+			dbURI += fmt.Sprintf("&sslkey=%s", db.clientKey)
+		}
+	}
+
+	return dbURI
 }
