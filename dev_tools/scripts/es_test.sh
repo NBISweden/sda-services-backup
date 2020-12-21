fd4c69484dd069475bedd3ac39a177d0d60fb08c
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2020-12-21 12:26:11 +0100

    Create ES index via curl for tests

--- dev_tools/scripts/es_test.sh
+++ dev_tools/scripts/es_test.sh
@@ -2,22 +2,24 @@
 
 NOW=$(date '+%Y%m%d%H%M')
 
-CONFIGFILE="dev_tools/config.yaml" go run . --action es_create --name "$NOW-123"
-CONFIGFILE="dev_tools/config.yaml" go run . --action es_create --name "abc-$NOW"
+for i in {1..100}; do
+  name=$(head -c 10 /dev/urandom | base64)
+  text=$(head -c 30 /dev/urandom | base64)
+  curl -XPOST "http://127.0.0.1:9200/$NOW-123/_doc/$i" -d "{\"$name\": \"$text\" }" -H "Content-Type: application/json"
+done
 
 CONFIGFILE="dev_tools/config.yaml" go run . --action es_backup --name "*$NOW*"
 
 s3cmd ls -c dev_tools/s3conf s3://dumps/
 
-CONFIGFILE="dev_tools/config.yaml" ELASTIC_PORT=9201 go run . --action es_restore --name "$NOW-123-test.bup"
-CONFIGFILE="dev_tools/config.yaml" ELASTIC_PORT=9201 go run . --action es_restore --name "abc-$NOW-test.bup"
+CONFIGFILE="dev_tools/config.yaml" ELASTIC_PORT=9201 go run . --action es_restore --name "$NOW-123.bup"
 
 sleep 5
 
-COUNT_1=$(curl http://127.0.0.1:9200/$NOW-test/_count | jq .count)
-COUNT_2=$(curl http://127.0.0.1:9201/$NOW-test/_count | jq .count)
+COUNT_1=$(curl http://127.0.0.1:9200/$NOW-123/_count | jq .count)
+COUNT_2=$(curl http://127.0.0.1:9201/$NOW-123/_count | jq .count)
 
-if [ "$COUNT_1" != "$COUNT_2" ]; then
+if [ "$COUNT_1" != "$COUNT_2" ] || [ "$COUNT_1" != 100 ] || [ "$COUNT_2" != 100 ]; then
   echo "Something went wrong, the imported backup does not contain all documents"
   echo "ES1 contains $COUNT_1 indices while ES2 contains $COUNT_2 indeices"
   exit 1
