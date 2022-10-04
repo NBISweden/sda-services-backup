ea867652a9c9b59c7c19079c5ff794cedc58f4e1
Kostas Koumpouras <kkoumpouras@gmail.com>
2022-10-04 15:25:04 +0200

    Updates in db integrity test script
    - stops if the image for running the backup is not created
    - stops if the backup is not in the bucket
    - drops the database to ensure that the test entry is not there anymore
    - checks if the above is true

--- dev_tools/scripts/db_integrity_test.sh
+++ dev_tools/scripts/db_integrity_test.sh
@@ -6,18 +6,31 @@ docker exec db psql -U postgres -d test -c "INSERT INTO local_ega.main(submissio
 # Build, run and execute the pg_basebackup action
 docker container run --rm -i --name pg-backup --network=host $(docker build --build-arg USER_ID=$(id -u) -f dev_tools/Dockerfile-backup -q -t backup .) /bin/sda-backup --action pg_basebackup
 
-# Find the name of the copy in the S3
+# Find the name of the copy in the S3 and check the length
 DBCOPY=$(s3cmd -c dev_tools/s3conf ls s3://dumps/ | grep ".tar" | cut -d '/' -f4)
+DBCOPYLENGTH=`echo -n "$DBCOPY" | wc -m`
 
-# Checks if the backup image exists locally from the previous step, if not creates the image
-# Then gets the physical copy of the database from the S3
+# Find the name of the backup image created in the previous step and check the length
 IMAGENAME=$(docker images -q backup)
 NAMELENGTH=`echo -n "$IMAGENAME" | wc -m`
 
-if [ $NAMELENGTH = 0 ]; then
-    docker container run -v $(pwd)/tmp:/home --rm -i --name pg-backup --network=host $(docker build --build-arg USER_ID=$(id -u) -f dev_tools/Dockerfile-backup -q .) /bin/sda-backup --action pg_bb-restore --name "$DBCOPY"
+# Make sure that the image and the db copy exist before moving on
+if [ $NAMELENGTH = 0 ] || [ $DBCOPYLENGTH = 0 ]; then
+    echo "the image or the db copy is missing"
+    exit 1
+# Get the db copy from S3
 else
     docker container run -v $(pwd)/tmp:/home --rm -i --name pg-backup --network=host backup /bin/sda-backup --action pg_bb-restore --name "$DBCOPY"
+fi
+
+# Drop the database to make sure that the test entry is not there anymore
+docker exec db psql -U postgres -d postgres -c "DROP DATABASE test;"
+
+# Check that the database is not there anymore
+USER=$(docker exec db psql -U postgres -d test -tA -c "select submission_user from local_ega.main where submission_file_path = 'test.c4gh';") > /dev/null 2>&1
+if [ "$USER" = "dummy" ]; then
+    echo "Failed to drop database"
+    exit 1
 fi
 
 # Remove everything from the db
