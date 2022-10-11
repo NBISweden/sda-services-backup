d356673ae11cfa389e89b5a2ba77348974739cb9
Kostas Koumpouras <kkoumpouras@gmail.com>
2022-10-11 11:31:16 +0200

    Update script for testing integrity

--- dev_tools/scripts/db_integrity_test.sh
+++ dev_tools/scripts/db_integrity_test.sh
@@ -18,10 +18,10 @@ NAMELENGTH=`echo -n "$IMAGENAME" | wc -m`
 if [ $NAMELENGTH = 0 ] || [ $DBCOPYLENGTH = 0 ]; then
     echo "the image or the db copy is missing"
     exit 1
-# Get the db copy from S3
-else
-    docker container run -v $(pwd)/tmp:/home --rm -i --name pg-backup --network=host backup /bin/sda-backup --action pg_db-unpack --name "$DBCOPY"
 fi
+
+# Get the db copy from S3
+docker container run -v $(pwd)/tmp:/home --rm -i --name pg-backup --network=host backup /bin/sda-backup --action pg_db-unpack --name "$DBCOPY"
 
 # Drop the database to make sure that the test entry is not there anymore
 docker exec db psql -U postgres -d postgres -c "DROP DATABASE test;"
