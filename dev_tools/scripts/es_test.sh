44f46ce7339f99c5a1a659fd7dc1530dffbce626
Joakim Bygdell <joakim.bygdell@nbis.se>
2023-11-20 13:50:56 +0100

    [tests] fix shellsheck issues

--- dev_tools/scripts/es_test.sh
+++ dev_tools/scripts/es_test.sh
@@ -8,15 +8,14 @@ fi
 
 NOW=$(date '+%Y%m%d%H%M')
 
-for i in $(seq 1 $num); do
+for i in $(seq 1 "$num"); do
   name=$(head -c 10 /dev/urandom | base64)
   text=$(head -c 30 /dev/urandom | base64)
   curl -XPOST "http://127.0.0.1:9200/$NOW-123/_doc/$i" -d "{\"$name\": \"$text\" }" -H "Content-Type: application/json"
 done
 
-CONFIGFILE="dev_tools/config_elastic.yaml" go run . --action es_backup --name "*$NOW*"
 # bail early since if this fails the rest will fail also
-if [ $? != 0 ]; then
+if ! CONFIGFILE="dev_tools/config_elastic.yaml" go run . --action es_backup --name "*$NOW*"; then
   exit 1
 fi
 
@@ -29,14 +28,15 @@ fi
 
 s3cmd ls -c dev_tools/s3conf s3://dumps/
 
+echo "restoring index"
 CONFIGFILE="dev_tools/config_elastic.yaml" ELASTIC_PORT=9201 go run . --action es_restore --name "$NOW-123.bup"
 
 sleep 5
 
-COUNT_1=$(curl http://127.0.0.1:9200/$NOW-123/_count | jq .count)
-COUNT_2=$(curl http://127.0.0.1:9201/$NOW-123/_count | jq .count)
+COUNT_1=$(curl "http://127.0.0.1:9200/$NOW-123/_count" | jq .count)
+COUNT_2=$(curl "http://127.0.0.1:9201/$NOW-123/_count" | jq .count)
 
-if [ "$COUNT_1" != "$COUNT_2" ] || [ "$COUNT_1" != $num ] || [ "$COUNT_2" != $num ]; then
+if [ "$COUNT_1" != "$COUNT_2" ] || [ "$COUNT_1" != "$num" ] || [ "$COUNT_2" != "$num" ]; then
   echo "Something went wrong, the imported backup does not contain all documents"
   echo "ES1 contains $COUNT_1 indices while ES2 contains $COUNT_2 indeices"
   exit 1
